7cc587e35a2451d61a0876dc75967d8a3b7262bf
oguni-yoshihiro <38971031+oguni-yoshihiro@users.noreply.github.com>
2018-12-23 03:02:03 +0900

    rpg2018サイトの、記事追記とスマホでの利便性向上

--- 2018/frpg/babel_dist/main.js
+++ 2018/frpg/babel_dist/main.js
@@ -1,19 +1,19 @@
 "use strict";
 
 {
-  var links = ["top_page", "manual", "get_lost", "distribution", "about", "contact"]; // 各ページ移動ボタンのクリックイベント
-
-  links.forEach(function (selector) {
-    document.querySelector("#" + selector).addEventListener("click", function () {
-      if (typeof article[selector] !== "string") {
+  // 各ページ移動ボタンのクリックイベント
+  Array.prototype.slice.call(document.querySelectorAll(".sidebar-item"), 0).forEach(function (sidebarItem) {
+    var id = sidebarItem.id;
+    sidebarItem.addEventListener("click", function () {
+      if (typeof article[id] !== "string") {
         console.error("typeof article[selector] !== \"string\"");
         return;
       }
 
-      if (/^\s*$/.test(article[selector])) {
+      if (/^\s*$/.test(article[id])) {
         document.querySelector("#article").innerHTML = "";
       } else {
-        document.querySelector("#article").innerHTML = "<div class=\"card card-preload scale-when-hover markdown-body\">" + marked(article[selector]) + "</div>";
+        document.querySelector("#article").innerHTML = "<div class=\"card card-preload scale-when-hover markdown-body\">" + marked(article[id].trim()) + "</div>";
         document.body.offsetWidth; // 強制的にレンダリング
         // IE対応のためのslice.call
 
@@ -24,7 +24,7 @@
     });
   }); // 背景読み込み
 
-  var secondImage = "./img/home_prairie.gif";
+  var secondImage = "./img/second_background_anime.gif";
   var img = new Image();
 
   img.onload = function () {
